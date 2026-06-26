3564ed3150aa7810b28e82c0ae39985fa1b3143f
AntonSarz <felicitolsuarez@gmail.com>
2026-06-26 15:06:25 -0400

    comentario mas claro sobre la sesion

--- Frontend/JS/sesion.js
+++ Frontend/JS/sesion.js
@@ -1,5 +1,15 @@
-/* --- Devuelve los datos del usuario logueado, o redirige al login si no hay sesion activa --- */
+/*──────────────────────────────────────────────────────────────────
+ MÓDULO DE GESTIÓN DE SESIÓN
+────────────────────────────────────────────────────────────────── */
+//Este script actúa en el cliente para validar el estado de autenticacion
+//Utiliza la API de Web Storage (sessionStorage) para garantizar el
+//el aislamiento de la sesión por pestaña y proteger las vistas
+//privadas (Catálogo y Perfil) de accesos no autorizados.
+
+// Obtiene el objeto del usuario autenticado, si no existe este registro, redirige al login.
 function obtenerUsuario() {
+
+    // Recupera la cadena estructurada en JSON del objeto de sesión
     const datos = sessionStorage.getItem('usuario');
 
     // Si no hay sesión activa, redirige al login
@@ -8,11 +18,16 @@ function obtenerUsuario() {
         return null;
     }
 
+    // Si existe, convierte la cadena plana en un objeto JSON manipulable por JS,
+    // permitiendo que los componentes puedan extraer el id_usuario
     return JSON.parse(datos);
 }
 
 // Cierra la sesion y redirige al login
 function cerrarSesion() {
+    // Remueve la clave 'usuario de la memoria
     sessionStorage.removeItem('usuario');
+
+    // redirecciona al login
     window.location.href = 'login.html';
 }
